a2bdadee07de242507a81b04d71f3fbdb0b383c0
Nishant Sinha <nishant@magictiger.com>
2016-05-07 11:07:10 +0530

    update

--- sent2labels/data_utils.py
+++ sent2labels/data_utils.py
@@ -60,21 +60,7 @@ def get_linear_tags (label2exprs):
 
 
 
-
-
-
-
-
-
-def getData():
-	label2exprs = extract_wit_ai_data(labels)
-#	pprint (label2exprs)
-	label2tokens = get_linear_tags(label2exprs)
-
-	#pprint(label2tokens)
-
-
-
+#returns X = [list of words] Y = [ list of labels ]
 def get_X_Y_data ():
 	label2exprs = extract_wit_ai_data(labels)
 #	pprint (label2exprs)
@@ -108,6 +94,49 @@ def get_X_Y_data ():
 		Y.append(currY)
 
 	return X, Y
+
+
+# Special vocabulary symbols - we always put them at the start.
+_PAD = b"_PAD"
+_GO = b"_GO"
+_EOS = b"_EOS"
+_UNK = b"_UNK"
+_START_VOCAB = [_PAD, _GO, _EOS, _UNK]
+
+PAD_ID = 0
+GO_ID = 1
+EOS_ID = 2
+UNK_ID = 3
+
+# Regular expressions used to tokenize.
+_WORD_SPLIT = re.compile(b"([.,!?\"':;)(])")
+_DIGIT_RE = re.compile(br"\d")
+
+def basic_tokenizer(sentence):
+  """Very basic tokenizer: split the sentence into a list of tokens."""
+  words = []
+  for space_separated_fragment in sentence.strip().split():
+    words.extend(re.split(_WORD_SPLIT, space_separated_fragment))
+  return [w for w in words if w]
+
+def make_vocab (word_lists, normalize_digits):
+	for wl in word_lists:
+		for w in wl:
+          word = re.sub(_DIGIT_RE, b"0", w) if normalize_digits else w
+          if word in vocab:
+            vocab[word] += 1
+          else:
+            vocab[word] = 1
+      vocab_list = _START_VOCAB + sorted(vocab, key=vocab.get, reverse=True)
+      if len(vocab_list) > max_vocabulary_size:
+        vocab_list = vocab_list[:max_vocabulary_size]
+
+def sent_to_token_ids (sentence, vocab, normalize_digits=True):
+	if not normalize_digits:
+    	return [vocabulary.get(w, UNK_ID) for w in words]
+  	# Normalize digits by 0 before looking words up in the vocabulary.
+  	return [vocabulary.get(re.sub(_DIGIT_RE, b"0", w), UNK_ID) for w in words]
+
 
 X, Y = get_X_Y_data ()
 print zip(X, Y)
